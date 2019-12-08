28bfba27c6d3d73b61a8f8abf60b5d4ab3cfd0aa
Julian Kimura <JulianKimura@dhcp-10-250-93-77.harvard.edu>
2019-12-08 17:36:12 -0500

    takes salmon output and extracts num_reads

---
+++ quant_file_extract.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import sys
+quant_file = sys.argv[1]
+
+with open(quant_file, 'r') as inputfile, open('count_file.txt', 'w') as outputfile:
+	for line in inputfile:
+		line = line.rstrip()
+		line_list = line.split('\t')
+		gene_name = line_list[0]
+		num_reads = line_list[4]
+		outputfile.write(gene_name+'\t'+num_reads+'\n')
+
+
+
