ba044447a449bc403e480abc187d9199b4c42219
Julian Kimura <JulianKimura@dhcp-10-250-93-77.harvard.edu>
2019-12-09 16:31:02 -0500

    added comments

--- combining_countfiles.py
+++ combining_countfiles.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python3
 
+#this script will take any number of files from the command line. 
+#the script assumes that the input will have gene names as 1st column
+#and num_reads on the 2nd column
+
+ 
+
 import sys
 import re
 
+#this block of code will parse through all of the files provided, and store the information into a dictionary of lists.
+#the key containing the gene names is 'gene_names'
+#the key containing num_reads will be the name of the input file that the reads came from with a number after it.
 num_reads_list = []
 file_number = 0
 mydict = {}
@@ -20,18 +29,25 @@ with open('combined_counts.txt', 'w') as outputfile:
 		mydict['gene_names'] = gene_name_list
 		mydict[files+str(file_number)] = num_reads_list
 
-#adds the header
+#the following block will take the information stored in the dictionary
+#and then write them out into the output file.
+#this portion adds the header
 	for index in mydict:
 		outputfile.write(index+'\t')
 	outputfile.write('\n')
-#looping through the indices 
+#looping through the indices
+#specifically, loops through for the number of items there are in the gene_names list
+#then takes the 1st entry for each list of each key such that the 1st column will be gene name
+#2nd column will be the numreads from one of the files, etc.... 
 	for x in range(len(mydict['gene_names'])):
 		for index in mydict:
 			outputfile.write(mydict[index][x])
 			outputfile.write('\t')
+#at the end of each line, a new line character is inputted
 		outputfile.write('\n')
 
-
+#this block of code will take the combined_counts.txt file produced from the code above
+#then will remove all genes with zero expression values. 
 with open('combined_counts.txt', 'r') as inputfile, open('combined_counts_filtered.txt', 'w') as output2:
 	for line in inputfile:
 		line = line.rstrip()
