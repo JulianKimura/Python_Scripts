899a024a97167487d08b531685c8847fdef4585b
Julian Kimura <JulianKimura@dhcp-10-250-93-77.harvard.edu>
2019-12-09 16:54:45 -0500

    debugged

--- quant_file_extract.py
+++ quant_file_extract.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 
 import sys
-quant_file = sys.argv[1:]
 
-with open(quant_file, 'r') as inputfile, open('count_file.txt', 'w') as outputfile:
-	for line in inputfile:
-		line = line.rstrip()
-		line_list = line.split('\t')
-		gene_name = line_list[0]
-		num_reads = line_list[4]
-		outputfile.write(gene_name+'\t'+num_reads+'\n')
+for files in sys.argv[1:]:
+	with open(files, 'r') as inputfile, open(files+'_counts', 'w') as outputfile:
+		for line in inputfile:
+			line = line.rstrip()
+			line_list = line.split('\t')
+			gene_name = line_list[0]
+			num_reads = line_list[4]
+			outputfile.write(gene_name+'\t'+num_reads+'\n')
 
 
