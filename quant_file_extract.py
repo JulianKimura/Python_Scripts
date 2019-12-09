5459d50130544f6049cb2439f074c3859ba53d32
Julian Kimura <JulianKimura@dhcp-10-250-93-77.harvard.edu>
2019-12-09 11:42:36 -0500

    now takes multiple files

--- quant_file_extract.py
+++ quant_file_extract.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import sys
-quant_file = sys.argv[1]
+quant_file = sys.argv[1:]
 
 with open(quant_file, 'r') as inputfile, open('count_file.txt', 'w') as outputfile:
 	for line in inputfile:
@@ -10,6 +10,5 @@ with open(quant_file, 'r') as inputfile, open('count_file.txt', 'w') as outputfi
 		gene_name = line_list[0]
 		num_reads = line_list[4]
 		outputfile.write(gene_name+'\t'+num_reads+'\n')
-
 
 
